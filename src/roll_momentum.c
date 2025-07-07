54951a9f20d5ecb00b0db36873f78f8e05befe20
keanine <keanen195@gmail.com>
2025-07-07 23:38:18 +0100

    Fixes for 1.1.0

--- src/roll_momentum.c
+++ src/roll_momentum.c
@@ -7,6 +7,13 @@
 
 #define ENABLE_DUST_PARTICLES (bool)recomp_get_config_u32("dust_particles")
 #define ENABLE_BUNNY_ROLL (bool)recomp_get_config_u32("bunny_roll")
+#define ENABLE_ROLL_TURNING (bool)recomp_get_config_u32("roll_turning")
+
+typedef enum PlayerActionInterruptResult {
+    /* -1 */ PLAYER_INTERRUPT_NONE = -1,
+    /*  0 */ PLAYER_INTERRUPT_NEW_ACTION,
+    /*  1 */ PLAYER_INTERRUPT_MOVE
+} PlayerActionInterruptResult;
 
 Vec3f D_8085D270 = { 0.0f, 0.04f, 0.0f };
 Color_RGBA8 D_8085D26C = { 255, 255, 255, 128 };
@@ -18,6 +25,8 @@ extern bool func_8083FE38(Player* this, PlayState* play);
 extern s32 func_80840A30(PlayState* play, Player* this, f32* arg2, f32 arg3);
 extern s32 Player_ActionHandler_7(Player* this, PlayState* play);
 extern void func_8083CB58(Player* this, f32 arg1, s16 arg2);
+extern void func_80836A5C(Player* this, PlayState* play);
+extern PlayerActionInterruptResult Player_TryActionInterrupt(PlayState* play, Player* this, SkelAnime* skelAnime, f32 frameRange);
 
 extern s16 sControlStickWorldYaw;
 extern FloorProperty sPrevFloorProperty;
@@ -57,8 +66,14 @@ RECOMP_HOOK("Player_Action_26") void on_Player_Action_Rolling(Player* this, Play
 }
 
 RECOMP_HOOK_RETURN("Player_Action_26") void return_Player_Action_Rolling() {
-
     if (mIsRolling) {
+        if (mThis->actionFunc != Player_Action_26) {
+            mIsRolling = false;
+        }
+    }
+    if (mIsRolling) {
+
+
         s16 sTurnStrength = BINANG_SUB(sControlStickWorldYaw, mThis->actor.shape.rot.y) / 10;
         f32 speedTarget = CLAMP(mCurrentSpeed, 0, ((mThis->transformation == PLAYER_FORM_ZORA) ? 6.0f : 5.5f));
         s16 yawTarget = mThis->actor.shape.rot.y;
@@ -69,7 +84,7 @@ RECOMP_HOOK_RETURN("Player_Action_26") void return_Player_Action_Rolling() {
             speedTarget *= 1.25f;
         }
 
-        if (sControlStickMagnitude != 0.0f) {
+        if (sControlStickMagnitude != 0.0f && ENABLE_ROLL_TURNING) {
             yawTarget += sTurnStrength;
         }
 
@@ -77,6 +92,7 @@ RECOMP_HOOK_RETURN("Player_Action_26") void return_Player_Action_Rolling() {
             (mThis->controlStickDirections[mThis->controlStickDataIndex] == PLAYER_STICK_DIR_BACKWARD)) {
             return;
         }
+
         func_8083CB58(mThis, speedTarget, yawTarget);
 
         if (ENABLE_DUST_PARTICLES) {
@@ -123,8 +139,8 @@ RECOMP_HOOK("func_8083827C") void func_8083827C(Player* this, PlayState* play) {
                         (sPlayerYDistToFloor > 20.0f) && (this->meleeWeaponState == PLAYER_MELEE_WEAPON_STATE_0)) {
                         
                         if ((ABS_ALT(angle) < 0x2000) && (this->speedXZ > 3.0f)) {
-                            if (mThis->actionFunc == Player_Action_26 && mBunnyHoodActive) {
-                                this->speedXZ = CLAMP(this->speedXZ, -100, ((mThis->transformation == PLAYER_FORM_ZORA ? 6.0f : 5.5f) * 1.5f) - 0.1f);
+                            if (this->actionFunc == Player_Action_26 && mBunnyHoodActive) {
+                                this->speedXZ = CLAMP(this->speedXZ, -100, ((this->transformation == PLAYER_FORM_ZORA ? 6.0f : 5.5f) * 1.5f) - 0.1f);
                             }
                         }
                     }
--- thunderstore/CHANGELOG.md
+++ thunderstore/CHANGELOG.md
@@ -1,3 +1,8 @@
 # Changelog
+## 1.1.0
+- Fixed a crash when jumping off a ledge
+- Fixed a bug where momentum was stored during the item get cutscene
+- Added a config option to disable roll turning (useful for speedrun tech!)
+
 ## 1.0.0
 - First Release
